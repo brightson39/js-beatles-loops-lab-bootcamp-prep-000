2580a5fca3c755f167c84768c340697b5d59c372
Bright Olayinka <explorebright@gmail.com>
2019-03-02 06:54:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-// add solution here
-function theBeatlesPlay(musicians, instruments){
-}
-
-
- var ();
- for theBeatlesPlay.musicians
-}
-var = ();
-
-  
-}
-
 function theBeatlesPlay(musicians, instruments) {
   var phrases = [];
   for (var i = 0; i < musicians.length; i++) {
